3e8ae5e2c26e21a73c38114d76163c0cb47aefa1
farhat1926 <farhatmoh2000@gmail.com>
2026-07-08 22:32:18 +0700

    penambahan tab ppi

--- src/pages/Dokumen.jsx
+++ src/pages/Dokumen.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { units } from "../data/units";
+// import { units } from "../data/units";
 
 export default function Dokumen() {
   const [selectedUnit, setSelectedUnit] = useState(null);
