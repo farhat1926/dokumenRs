e4e717c61ba13782aa4a9df1e690808d0f693950
farhat1926 <farhatmoh2000@gmail.com>
2026-07-08 22:15:08 +0700

    penambahan ppi

--- src/pages/ppi.jsx
+++ src/pages/ppi.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 
-export default function Medis() {
+export default function Ppi() {
   const [selectedUnit, setSelectedUnit] = useState(null);
   const [selectedKategori, setSelectedKategori] = useState(null);
   const [password, setPassword] = useState("");
